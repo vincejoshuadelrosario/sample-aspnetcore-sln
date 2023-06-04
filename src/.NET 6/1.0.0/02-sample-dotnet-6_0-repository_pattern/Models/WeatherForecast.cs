5c48dea9abb735a4c4c72141909df1227adf4d07
vince.j.del.rosario <vince.j.del.rosario@accenture.com>
2023-06-04 23:40:37 +0800

    1.0.0 | .NET 6 | rename folder sequence

---
+++ src/.NET 6/1.0.0/02-sample-dotnet-6_0-repository_pattern/Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace sample_dotnet_6_0.Models
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
