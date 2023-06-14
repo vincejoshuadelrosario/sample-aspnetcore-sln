c165d3865aff6a6e1823fa000f7a0629bec56bad
vince.j.del.rosario <vince.j.del.rosario@accenture.com>
2023-06-15 04:01:55 +0800

    1.0.0 | .NET 6 | Web API | with IMemoryCache

---
+++ src/.NET 6/1.0.0/sample-dotnet-6_0-web-api/04-memory-cache/Repositories/Cache/EmployeesCacheRepository.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using sample_dotnet_6_0.Data;
+using sample_dotnet_6_0.Models;
+using System.Collections.Concurrent;
+
+namespace sample_dotnet_6_0.Repositories.Cache
+{
+    internal sealed class EmployeesCacheRepository : ICacheRepository<Employee>
+    {
+        private static ConcurrentDictionary<Guid, Employee>? EmployeesCache;
+
+        private readonly IMemoryCache _memoryCache;
+        private readonly IDataAccess<Employee> _dataAccess;
+
+        public EmployeesCacheRepository(
+            IMemoryCache memoryCache,
+            IDataAccess<Employee> dataAccess)
+        {
+            _memoryCache = memoryCache;
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAsync()
+        {
+            await LoadCache();
+
+            return EmployeesCache?.Values.AsEnumerable() ?? Enumerable.Empty<Employee>();
+        }
+
+        public async Task<Employee?> GetAsync(Guid id)
+        {
+            await LoadCache();
+
+            Employee? employee = null;
+            EmployeesCache?.TryGetValue(id, out employee);
+
+            return employee;
+        }
+
+        public async Task<bool> AddAsync(Employee newEmployee)
+        {
+            await LoadCache();
+
+            return EmployeesCache?.TryAdd(newEmployee.Id, newEmployee) ?? false;
+        }
+
+        public async Task<bool> AddAsync(IEnumerable<Employee> employees)
+        {
+            await LoadCache();
+
+            var result = false;
+
+            foreach (var employee in employees)
+            {
+                result = EmployeesCache?.TryAdd(employee.Id, employee) ?? false;
+                if (!result) break;
+            }
+
+            return result;
+        }
+
+        public async Task<bool> UpdateAsync(Employee updatedEmployee)
+        {
+            await LoadCache();
+
+            Employee? oldEmployee = null;
+            EmployeesCache?.TryGetValue(updatedEmployee.Id, out oldEmployee);
+
+            if (oldEmployee is null) return false;
+
+            return EmployeesCache?.TryUpdate(updatedEmployee.Id, updatedEmployee, oldEmployee) ?? false;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            await LoadCache();
+            
+            return EmployeesCache?.TryRemove(id, out _) ?? false;
+        }
+
+        private async Task LoadCache()
+        {
+            EmployeesCache = _memoryCache.Get<ConcurrentDictionary<Guid, Employee>>("employees");
+
+            if (EmployeesCache is null)
+            {
+                await Task.Delay(3000);
+
+                EmployeesCache = new ConcurrentDictionary<Guid, Employee>();
+
+                var employees = await _dataAccess.GetAsync();
+
+                foreach(var employee in employees)
+                {
+                    EmployeesCache.TryAdd(employee.Id, employee);
+                }
+
+                _memoryCache.Set("employees", EmployeesCache, TimeSpan.FromSeconds(60));
+            }
+        }
+    }
+}
---
+++ src/.NET 6/1.0.0/sample-dotnet-6_0-web-api/04-memory-cache/Services/Cache/EmployeesCacheService.cs
@@ -0,0 +1,58 @@
+﻿using sample_dotnet_6_0.Dto;
+using sample_dotnet_6_0.Mappers;
+using sample_dotnet_6_0.Models;
+using sample_dotnet_6_0.Repositories.Cache;
+
+namespace sample_dotnet_6_0.Services.Cache
+{
+    public class EmployeesCacheService : IEmployeesCacheService
+    {
+
+        private readonly ICacheRepository<Employee> _employeesCacheRepository;
+
+        public EmployeesCacheService(ICacheRepository<Employee> employeesCacheRepository)
+        {
+            _employeesCacheRepository = employeesCacheRepository;
+        }
+
+        public async Task<IEnumerable<EmployeeReadDto>> GetAsync() =>
+            (await _employeesCacheRepository.GetAsync()).Select(e => e.ToEmployeeReadDto());
+
+        public async Task<EmployeeReadDto?> GetAsync(Guid id)
+        {
+            if (id == Guid.Empty) return null;
+
+            var result = await _employeesCacheRepository.GetAsync(id);
+
+            return result?.ToEmployeeReadDto();
+        }
+
+        public async Task<bool> AddAsync(EmployeeNewDto employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            return await _employeesCacheRepository.AddAsync(employee.ToEmployee());
+        }
+
+        public async Task<bool> AddAsync(IEnumerable<EmployeeNewDto> employees)
+        {
+            if (employees is null) throw new ArgumentNullException(nameof(employees));
+
+            return await _employeesCacheRepository.AddAsync(employees.Select(e => e.ToEmployee()));
+        }
+
+        public async Task<bool> UpdateAsync(EmployeeUpdateDto employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            return await _employeesCacheRepository.UpdateAsync(employee.ToEmployee());
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            if (id == Guid.Empty) throw new ArgumentNullException(nameof(id));
+
+            return await _employeesCacheRepository.DeleteAsync(id);
+        }
+    }
+}
